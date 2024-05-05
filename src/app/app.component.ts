91df8d1772871e0b1cd589bf6423cdcf3db0c9cb
Joffre C <joffre.condor@epn.edu.ec>
2024-05-05 18:27:44 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BarComponent} from "./components/bar/bar.component";
+import {PieComponent} from "./components/pie/pie.component";
+import {ScatterComponent} from "./components/scatter/scatter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BarComponent, PieComponent, ScatterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
